88b55baf0f9622e99144e36d6c1e3d965361a1c7
kitamura-k <kitamura-k@gnavi.co.jp>
2018-10-31 19:46:51 +0900

    修正

--- gulpfile.js
+++ gulpfile.js
@@ -37,13 +37,21 @@ gulp.task('svg', function() {
     .pipe(gulp.dest('dist/svg/'));
 });
 
+gulp.task('js', function() {
+    gulp.src(
+        ['src/js/*.js']
+    )
+    .pipe(gulp.dest('dist/js/'));
+});
+
 gulp.task('bs-reload', function () {
     browserSync.reload();
 });
 
 // src 配下の *.html, *.css ファイルが変更されたリロード。
-gulp.task('default', ['browser-sync', 'ejs', 'sass', 'svg'], function () {
+gulp.task('default', ['browser-sync', 'ejs', 'sass', 'svg', 'js'], function () {
     gulp.watch('src/**/*.ejs', ['bs-reload', 'ejs']);
     gulp.watch('src/**/*.scss', ['bs-reload', 'sass']);
     gulp.watch('src/**/*.svg', ['bs-reload', 'svg']);
+    gulp.watch('src/**/*.js', ['bs-reload', 'js']);
 });
